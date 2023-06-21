5bf14363b8b96fe1d65aaa0ba232553524c559cf
stupendous-web <topher@stupendousweb.com>
2023-06-21 16:53:52 -0700

    Think twice, code once.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/utils/chakra";
+import {Analytics} from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+        <Analytics />
     </ChakraProvider>
   );
 }
