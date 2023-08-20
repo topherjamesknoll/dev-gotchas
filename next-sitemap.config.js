08f4439566428f075c8f8dd388573763979cc605
stupendous-web <topher@stupendousweb.com>
2023-08-20 08:30:17 -0700

    Think twice, code once.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,26 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 
-// const axios = require("axios");
-
 module.exports = {
-    siteUrl: "https://devgotchas.com",
-    generateRobotsTxt: true,
-    generateIndexSitemap: false,
-    autoLastmod: false,
-    priority: false,
-    changefreq: "monthly",
-    // additionalPaths: async () => {
-    //     const result = [];
-    //     await axios
-    //         .get("https://public-api.wordpress.com/rest/v1.1/sites/220124193/posts")
-    //         .then((response) => {
-    //             response.data?.posts?.map((post) => {
-    //                 result.push({
-    //                     loc: `/${post.slug}`,
-    //                 });
-    //             });
-    //         });
-    //
-    //     return result;
-    // },
+  siteUrl: "https://devgotchas.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  autoLastmod: false,
+  priority: false,
+  changefreq: "monthly",
 };
