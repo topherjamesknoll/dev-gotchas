7a7d05fed3cf270425ff624a92a705511a9f6284
stupendous-web <topher@stupendousweb.com>
2023-08-27 13:21:08 -0700

    Think twice, code once.

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,48 @@
 import { Analytics } from "@vercel/analytics/react";
 import { ChakraBaseProvider } from "@chakra-ui/react";
 import theme from "@/utils/chakra";
+import Script from "next/script";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [isProduction, setIsProduction] = useState(false);
+
+  useEffect(() => {
+    window.location.hostname === "devgotchas.com" && setIsProduction(true);
+    console.log(
+      "%c🤯 DEV GOTCHAS\ntopher@stupendousweb.com\n",
+      "color: #d02670"
+    );
+  }, []);
+
   return (
-    <ChakraBaseProvider theme={theme}>
-      <Component {...pageProps} />
-      <Analytics />
-    </ChakraBaseProvider>
+    <div>
+      {isProduction && (
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy={"afterInteractive"}
+        />
+      )}
+      <Head>
+        {isProduction && (
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+              });
+            `}
+          </script>
+        )}
+        <meta name={"viewport"} content={"width=device-width"} />
+      </Head>
+      <ChakraBaseProvider theme={theme}>
+        <Component {...pageProps} />
+        <Analytics />
+      </ChakraBaseProvider>
+    </div>
   );
 }
