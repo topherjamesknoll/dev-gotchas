24bc7098475e8cacc89d488f8ed992a315faa58d
stupendousweb <topher@stupendousweb.com>
2024-02-11 10:59:18 -0800

    Think twice, code once.

--- pages/my-favorite-expo-and-eas-commands-for-react-native-development.js
+++ pages/my-favorite-expo-and-eas-commands-for-react-native-development.js
@@ -1,4 +1,4 @@
-import { Text } from "@geist-ui/core";
+import { Snippet, Text } from "@geist-ui/core";
 import Head from "next/head";
 import Layout from "@/components/Layout";
 
@@ -14,6 +14,43 @@ export default function MyFavoriteExpoAndEasCommandsForReactNativeDevelopment() 
       <Layout>
         <Text h1>
           My Favorite Expo and EAS Commands for React Native Development
+        </Text>
+        <Text>
+          These are some commands I use very often for developing React Native
+          apps with Expo. Keep in mind some additional packages such as
+          expo-dev-client and expo-updates may be required. You may also need to
+          update your eas.json channels.
+        </Text>
+        <Text h2>Previewing your App Locally</Text>
+        <Snippet>expo start -c</Snippet>
+        <Text>
+          The -c flag just clears the cache which I find helpful. In the latest
+          version of Expo it will prompt you to run the development build or
+          inside Expo Go. You can also just add the –dev-client flag to your
+          command if you’d like to run your app in your development build.
+        </Text>
+        <Text h2>Creating a Dev Build</Text>
+        <Snippet>eas build --profile development</Snippet>
+        <Text h2>Creating a Dev Build for iOS Simulators</Text>
+        <Snippet>
+          eas build --profile development-simulator --platform ios
+        </Snippet>
+        <Text>
+          This will require a separate “development-simulator” profile with some
+          additional configuration inside your eas.json file.
+        </Text>
+        <Text h2>ESA Updates</Text>
+        <Snippet>eas update</Snippet>
+        <Text>
+          Updates are self explanatory but can be used, also to distribute your
+          app internally whiteout having to run a local server. You will need
+          expo-updates installed for this one.
+        </Text>
+        <Text h2>Submit your App to the GooglePlay and App Stores</Text>
+        <Snippet>eas submit</Snippet>
+        <Text>
+          This will bundle your build and send it to the appropriate store for
+          people to download and enjoy!
         </Text>
       </Layout>
     </>
