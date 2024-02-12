188a9adfec0fe8da787785a4cf7b5a4b10706816
stupendousweb <topher@stupendousweb.com>
2024-02-12 07:28:12 -0800

    Think twice, code once.

--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,34 @@
+import { Fragment } from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { CssBaseline } from "@geist-ui/core";
-import { Fragment } from "react";
 
-Document.getInitialProps = async (ctx) => {
-  const initialProps = await Document.getInitialProps(ctx);
-  const styles = CssBaseline.flush();
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = CssBaseline.flush();
 
-  return {
-    ...initialProps,
-    styles: [
-      <Fragment key="1">
-        {initialProps.styles}
-        {styles}
-      </Fragment>,
-    ],
-  };
-};
+    return {
+      ...initialProps,
+      styles: [
+        <Fragment key="1">
+          {initialProps.styles}
+          {styles}
+        </Fragment>,
+      ],
+    };
+  }
 
-export default function MyDocument() {
-  return (
-    <Html lang={"en"}>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+  render() {
+    return (
+      <Html lang={"en"}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
